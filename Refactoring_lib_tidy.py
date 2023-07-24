3d54b612002f8f32b4833db00cfcfba86df1546b
Elshan's Projects <89624267+Elshan-Projects@users.noreply.github.com>
2023-07-24 13:25:19 -0400

    blob_based Refactoring_lib_tidy.py

--- Refactoring_lib_tidy.py
+++ Refactoring_lib_tidy.py
@@ -1,106 +1,101 @@
 import cv2
 import numpy as np
 import os
+import sqlite3
 
-# Function to get all file paths in a directory
-def get_file_paths(directory):
-    return [os.path.join(root, file_name) 
-            for root, _, files in os.walk(directory) 
-            for file_name in files]
+class ImageFeatureDataset:
+    def __init__(self, db_name='image_features.db'):
+        self.db_name = db_name
+        self.conn = self.create_connection()
+        self.cursor = self.conn.cursor()
+        self.create_tables()
 
-# Function to save data as numpy array
-def save_as_numpy_array(data, path):
-    try:
-        np.save(path, data)
-        return path, str(data.dtype)
-    except Exception as e:
-        print(f"Error saving data as numpy array: {e}")
-        return None
+    def __del__(self):
+        self.close_connection()
 
-# Function to load an image and its extension
-def load_image(image_path):
-    try:
-        image = cv2.imread(image_path)
-        return image, os.path.splitext(image_path)[1][1:]
-    except Exception as e:
-        print(f"Error loading image: {e}")
-        return None, None
+    def create_connection(self):
+        try:
+            conn = sqlite3.connect(self.db_name)
+            return conn
+        except sqlite3.Error as e:
+            print(f"Error connecting to the database: {e}")
+            return None
 
-# Function to get the dimensions of an image
-def get_image_dimensions(image):
-    if image is not None:
-        return image.shape[0], image.shape[1], image.shape[2]
-    return None, None, None
+    def close_connection(self):
+        try:
+            if self.conn:
+                self.cursor.close()
+                self.conn.close()
+        except sqlite3.Error as e:
+            print(f"Error closing the database connection: {e}")
 
-# Function to compute color histogram of an image
-def compute_color_histogram(image):
-    try:
-        hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-        return cv2.normalize(hist, hist).flatten()
-    except Exception as e:
-        print(f"Error computing color histogram: {e}")
-        return None
+    def create_tables(self):
+        try:
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY, path TEXT, height INTEGER, width INTEGER, channels INTEGER, extension TEXT)")
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS features (id INTEGER PRIMARY KEY, image_id INTEGER, array_path TEXT, array_dtype TEXT, hist_path TEXT, edges_path TEXT, corners_path TEXT)")
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(f"Error creating the tables: {e}")
 
-# Function to perform edge detection on an image
-def detect_edges(image):
-    try:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        return cv2.Canny(gray, 30, 100)
-    except Exception as e:
-        print(f"Error detecting edges: {e}")
-        return None
+    def insert_image_and_features(self, image_path, image_data):
+        try:
+            binary_string = image_data.tobytes()
+            array_path, array_dtype = self.save_as_numpy_array(image_data, f'{image_path}_arr.npy')
+            hist_path, _ = self.save_as_numpy_array(self.compute_color_histogram(image_data), f'{image_path}_hist.npy')
+            edges_path, _ = self.save_as_numpy_array(self.detect_edges(image_data), f'{image_path}_edges.npy')
+            corners_path, _ = self.save_as_numpy_array(self.detect_corners(image_data), f'{image_path}_corners.npy')
+            self.cursor.execute("INSERT INTO images (path, height, width, channels, extension) VALUES (?, ?, ?, ?, ?)",
+                                (image_path, image_data.shape[0], image_data.shape[1], image_data.shape[2], os.path.splitext(image_path)[1][1:]))
+            image_id = self.cursor.lastrowid
+            self.cursor.execute("INSERT INTO features (image_id, array_path, array_dtype, hist_path, edges_path, corners_path) VALUES (?, ?, ?, ?, ?, ?)",
+                                (image_id, array_path, array_dtype, hist_path, edges_path, corners_path))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            self.conn.rollback()
+            print(f"Error inserting image and features: {e}")
 
-# Function to perform corner detection on an image
-def detect_corners(image):
-    try:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        return np.int0(cv2.goodFeaturesToTrack(gray, 25, 0.01, 10))
-    except Exception as e:
-        print(f"Error detecting corners: {e}")
-        return None
+    def get_all_images(self):
+        try:
+            self.cursor.execute("SELECT path FROM images")
+            return [row[0] for row in self.cursor.fetchall()]
+        except sqlite3.Error as e:
+            print(f"Error retrieving image paths: {e}")
+            return []
 
-# Function to extract features from an image
-def extract_features(image_path):
-    image, ext = load_image(image_path)
-    if image is None:
-        return None
-    height, width, channels = get_image_dimensions(image)
-    array_path_info = save_as_numpy_array(np.array(image), f'{image_path}_arr.npy')
-    hist_path_info = save_as_numpy_array(compute_color_histogram(image), f'{image_path}_hist.npy')
-    edges_path_info = save_as_numpy_array(detect_edges(image), f'{image_path}_edges.npy')
-    corners_path_info = save_as_numpy_array(detect_corners(image), f'{image_path}_corners.npy')
+    def get_image_and_features(self, image_path):
+        try:
+            self.cursor.execute("SELECT image.id, path, height, width, channels, extension, array_path, array_dtype, hist_path, edges_path, corners_path FROM images "
+                                "JOIN features ON images.id = features.image_id WHERE path=?", (image_path,))
+            row = self.cursor.fetchone()
+            if row:
+                image_id, _, height, width, channels, extension, array_path, array_dtype, hist_path, edges_path, corners_path = row
+                image_data = self.blob_to_numpy_array(array_path, array_dtype, (height, width, channels))
+                hist_data = self.blob_to_numpy_array(hist_path, np.float32)
+                edges_data = self.blob_to_numpy_array(edges_path, np.uint8)
+                corners_data = self.blob_to_numpy_array(corners_path, np.int0)
+                return image_data, hist_data, edges_data, corners_data, extension
+            else:
+                return None, None, None, None, None
+        except sqlite3.Error as e:
+            print(f"Error retrieving image and features: {e}")
+            return None, None, None, None, None
 
-    if None in [array_path_info, hist_path_info, edges_path_info, corners_path_info]:
-        return None
-    array_path, array_dtype = array_path_info
-    hist_path, _ = hist_path_info
-    edges_path, _ = edges_path_info
-    corners_path, _ = corners_path_info
-    return ((image_path, height, width, channels, ext), (array_path, array_dtype), (hist_path, edges_path, corners_path))
+    def save_as_numpy_array(self, data, path):
+        try:
+            np.save(path, data)
+            return path, str(data.dtype)
+        except Exception as e:
+            print(f"Error saving data as numpy array: {e}")
+            return None
 
-# Function to generate SQL insert statements
-def generate_sql_inserts(num, features):
-    main_sql = f"INSERT INTO Main_tab_ (Num, Relative_Location_, Height_, Width_, Channels_, File_Type_, Image_id_) VALUES {num}, %s, %s, %s, %s, %s, %s);"
-    array_sql = f"INSERT INTO Array_Tab_ (Num, Array_location_, Array_type_, Image_id_) VALUES {num}, %s, %s, %s);"
-    feature_sql = f"INSERT INTO Feature_Tab_ (Num, Histogram_, Edges_, Corners_, Image_id_) VALUES {num}, %s, %s, %s, %s);"
-    return main_sql, array_sql, feature_sql
-
-# Function to write SQL insert statements to a file
-def write_to_file(inserts):
-    try:
-        with open('Inserts.sql', 'w') as f:
-            for insert in inserts:
-                f.write(insert + '\n')
-        print("SQL insert statements written to 'Inserts.sql'.")
-    except Exception as e:
-        print(f"Error writing SQL insert statements to file: {e}")
-
-# Using the above functions to extract image features and generate SQL insert statements
-if __name__ == "__main__":
-    image_loc = 'path/to/your/image.png'  # Replace this with the path to your image
-    features = extract_features(image_loc)
-    if features is not None:
-        num = 8888
-        inserts = generate_sql_inserts(num, features)
-        write_to_file(inserts)
-
+    def blob_to_numpy_array(self, blob_path, dtype, shape=None):
+        try:
+            with open(blob_path, 'rb') as f:
+                binary_string = f.read()
+            if shape:
+                return np.frombuffer(binary_string, dtype=dtype).reshape(shape)
+            else:
+                return np.frombuffer(binary_string, dtype=dtype)
+        except Exception as e:
+            print(f"Error loading binary data to NumPy array: {e}")
+            return None
